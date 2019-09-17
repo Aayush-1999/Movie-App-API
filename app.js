bc040123a95cb0c78f247c1bbf5d6ba686e90937
Aayush Agarwal <aayushaggarwal2007@gmail.com>
2019-09-17 16:35:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-var express=require("express");
-var app=express();
-var request=require("request");
 require("dotenv").config();
+const express = require("express"),
+          app = express(),
+      request = require("request");
+
 app.set("view engine","ejs");
 
-app.get("/",function(req, res) {
+app.get("/",(req, res)=> {
     res.render("search");
 })
 
-app.get("/results",function(req,res){
-    var query=req.query.searchterm;
-    var url=process.env.API_KEY+query;
-   request(url,function(error,response,body){
+app.get("/results",(req,res)=>{
+    let query=req.query.searchterm;
+    let url=process.env.API_KEY+query;
+   request(url,(error,response,body)=>{
        var data=JSON.parse(body);
-       res.render("results",{data:data});
+       res.render("results",{data});
    }); 
 });
 
